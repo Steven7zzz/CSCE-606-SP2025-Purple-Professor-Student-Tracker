b88f5244f42c08fde37823b8f6111c962527cd76
evanwu-tamu <evanwuaccount@tamu.edu>
2025-02-09 16:12:23 -0600

    add a upload student form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,26 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to @student
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def student_params
+      params.expect(student: [ :uin, :first_name, :last_name, :major, :email ])
+    end
 end
