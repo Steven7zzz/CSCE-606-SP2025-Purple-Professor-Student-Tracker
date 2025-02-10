3bbc07d2b7e97af255f6c54306542372cd9ca0ec
Steven7zzz <q1824711081@gmail.com>
2025-02-09 18:24:12 -0600

    added jobs and mailers tests

---
+++ spec/jobs/jobs_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationJob, type: :job do
+    class TestJob < ApplicationJob
+      def perform(*args); end
+    end
+  
+    describe "job execution" do
+      it "executes successfully" do
+        expect { TestJob.perform_later }.to have_enqueued_job(TestJob)
+      end
+    end
+  end
---
+++ spec/mailers/application_mailer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationMailer, type: :mailer do
+    describe "default settings" do
+      it "has the correct default from address" do
+        expect(ApplicationMailer.default[:from]).to eq("from@example.com")
+      end
+    end
+  end
+  
