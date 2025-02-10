ea9fdd08ca021a949207c35d393bf1ec098bd99d
evanwu-tamu <evanwuaccount@tamu.edu>
2025-02-09 18:15:37 -0600

    Add student controller tests

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -13,3 +13,52 @@ RSpec.describe "Students", type: :request do
     end
   end
 end
+
+RSpec.describe "Student", type: :request do
+  describe "GET /students/:id" do
+    it "returns a successful response" do
+      student = Student.create(first_name: "John Doe")
+      get student_path(student)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include(student.first_name)
+    end
+  end
+end
+
+RSpec.describe "StudentsNew", type: :request do
+  describe "GET /students/new" do
+    it "returns a successful response" do
+      get new_student_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+RSpec.describe StudentsController, type: :controller do
+  describe 'POST #create' do
+    let(:valid_attributes) do
+      {
+        student: {
+          uin: '123456789',
+          first_name: 'John',
+          last_name: 'Doe',
+          major: 'Computer Science',
+          email: 'john.doe@example.com'
+        }
+      }
+    end
+
+    context 'with valid parameters' do
+      it 'creates a new Student' do
+        expect {
+          post :create, params: valid_attributes
+        }.to change(Student, :count).by(1)
+      end
+
+      it 'redirects to the created student' do
+        post :create, params: valid_attributes
+        expect(response).to redirect_to(Student.last)
+      end
+    end
+  end
+end
